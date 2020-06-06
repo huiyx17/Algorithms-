a61cfc6913ea6357e214fa1be84474226aa97329
huiyx17 <huiyx17@qq.com>
2020-06-06 22:18:10 +0800

    add 3 files

--- Algorithms/Insertion-sort.cpp
+++ Algorithms/Insertion-sort.cpp
@@ -1,2 +1,18 @@
+#include<iostream>
+#include<vector>
 
-hhhhhh
+using namespace std;
+class Solution{
+public:
+    //插入排序
+    void Insert_sortion(vector<int>& nums)
+    {
+        for (int j = 1; j < nums.size(); j++)
+        {
+            for (int i = j ; nums[i] > nums[i - 1] && i > 0; i--)
+            {
+                swap(nums[i], nums[i-1]);
+            }
+        }
+    }
+};
